95b6d3fac8d4e804e4ab163e0c402ecd050cc215
Kauê Pavan Silva <kauepavansilva48@gmail.com>
2026-03-09 14:51:54 -0300

    Match_Case_Código

---
+++ MatchCase.py
@@ -0,0 +1,14 @@
+comando = "mover", "norte", 140
+
+match comando:
+    case "decolar":
+        print("Subindo!")
+        
+  case "pousar":
+        print("Descendo!")
+        
+  case "girar", angulo: 
+        print(f"Girando o drone em {angulo} graus.")
+
+  case "mover", direcao, distancia:
+        print(f"Indo para o {direcao} por {distancia} metros.")
